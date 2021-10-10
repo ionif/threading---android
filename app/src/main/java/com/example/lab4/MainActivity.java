bd2cb4ee592804b64b871ab2cc248081aba9c230
ionif <aionkov@gmail.com>
2021-10-10 18:00:21 -0500

    threading lab

---
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button startButton;
+    private volatile boolean stopThread = false;
+    class ExampleRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            mockFileDownloader();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+    }
+
+    public void mockFileDownloader() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.setText("Downloading...");
+            }
+        });
+
+        for (int downloadProgress = 0; downloadProgress <= 100; downloadProgress=downloadProgress+10) {
+            if (stopThread) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startButton.setText("Start");
+                    }
+                });
+                return;
+            }
+            Log.d("MainActivity", "Download Progress: " + downloadProgress + "%");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startButton.setText("Start");
+            }
+        });
+    }
+
+    public void startDownload(View view) {
+        stopThread = false;
+        ExampleRunnable runnable = new ExampleRunnable();
+        new Thread(runnable).start();
+    }
+    public void stopDownload(View view) {
+        stopThread = true;
+    }
+}
